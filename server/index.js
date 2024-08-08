a7c325bdc90652beef973b5b8c9feb1d74838b58
JamieD <mtriet193@gmail.com>
2024-08-08 15:58:07 +0700

    fixing name folder

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import mongoose from "mongoose"
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+const databaseUrl = "1" + process.env.DATABASE_URL;
+
+app.use(cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH","DELETE"],
+    credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+
+const server = app.listen(port,()=>{
+    console.log(`Server is running at http://localhost:${port}`);
+    
+});
+
+mongoose.connect(databaseUrl)
+.then(()=>console.log("DB connection successful"))
+.catch((e)=>console.log(e.message));
