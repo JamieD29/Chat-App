eed05a6e0e2b76013a1cd1e765d09f01201b50fc
JamieD <mtriet193@gmail.com>
2025-02-17 17:05:07 +0700

    Profile image

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../models/UserModel.js";
 import { compare } from "bcrypt";
-
+import { renameSync, unlinkSync } from "fs";
 const maxAge = 3 * 24 * 60 * 60 * 1000;
 
 const createToken = (email, userId) => {
@@ -43,8 +43,8 @@ export const login = async (request, response, next) => {
     }
 
     const user = await User.findOne({ email });
-    console.log("Auth controller" , { user });
-    
+    console.log("Auth controller", { user });
+
     if (!user) {
       return response.status(400).send("User with the given email not found");
     }
@@ -79,20 +79,98 @@ export const login = async (request, response, next) => {
 export const getUserInfo = async (request, response, next) => {
   try {
     const userData = await User.findById(request.userId);
-    if(!userData){
-      return response.status(404).send("User with the given id is not found or not exist");
+    if (!userData) {
+      return response
+        .status(404)
+        .send("User with the given id is not found or not exist");
     }
     return response.status(200).json({
-      
-        id: userData.id,
-        email: userData.email,
-        firstName: userData.firstName,
-        lastName: userData.lastName,
-        image: userData.image,
-        color: userData.color,
-        profileSetup: userData.profileSetup,
-     
+      id: userData.id,
+      email: userData.email,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+      profileSetup: userData.profileSetup,
     });
+  } catch (err) {
+    console.log({ err });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+export const updateProfile = async (request, response, next) => {
+  try {
+    const { userId } = request;
+    const { firstName, lastName, color } = request.body;
+    // const userData = await User.findById(request.userId);
+    if (!firstName || !lastName) {
+      return response
+        .status(404)
+        .send("First name, last name and color are required");
+    }
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      { firstName, lastName, color, profileSetup: true },
+      { new: true, runValidators: true }
+    );
+
+    return response.status(200).json({
+      id: userData.id,
+      email: userData.email,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+      profileSetup: userData.profileSetup,
+    });
+  } catch (err) {
+    console.log({ err });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+export const addProfileImage = async (request, response, next) => {
+  try {
+    if (!request.file) {
+      return response.status(400).send("File is required");
+    }
+    const date = Date.now();
+    let fileName = "uploads/profiles" + date + request.file.originalname;
+    renameSync(request.file.path, fileName);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      request.userId,
+      { image: fileName },
+      { new: true, runValidators: true }
+    );
+
+    return response.status(200).json({
+      image: updatedUser.image,
+    });
+  } catch (err) {
+    console.log({ err });
+    return response.status(500).send("Internal Server Error");
+  }
+};
+
+export const removeProfileImage = async (request, response, next) => {
+  try {
+    const {userId} = request; 
+    const user = await User.findById(userId);
+
+    if(!user){
+      return response.status(404).send("User not found");
+    }
+
+    if(user.image){
+      unlinkSync(user.image);
+    }
+
+    user.image=null;
+    await user.save();
+
+    return response.status(200).msg("Profile image removed.");
   } catch (err) {
     console.log({ err });
     return response.status(500).send("Internal Server Error");
